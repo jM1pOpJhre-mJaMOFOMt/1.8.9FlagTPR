a1047f93e43925c0d8c5439f824d7a82e885b63d
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2020-08-04 03:18:57 -0700

    Initially commit

---
+++ src/main/java/com/nur/flagtpr/MyEventHandler.java
@@ -0,0 +1,23 @@
+package com.nur.flagtpr;
+
+
+import net.minecraft.event.ClickEvent;
+import net.minecraft.event.ClickEvent.Action;
+import net.minecraft.util.ChatStyle;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import java.util.regex.*;
+
+public class MyEventHandler {
+	@SubscribeEvent
+    public void onOtherChat(ClientChatReceivedEvent event)
+    {
+		String FLAG_REGEXP = "^\\[.+\\] (\\w+) VL \\d+";
+		Pattern FLAG_PATTERN = Pattern.compile(FLAG_REGEXP);
+		Matcher m = FLAG_PATTERN.matcher(event.message.getUnformattedText());
+		if(m.find()) {
+			ChatStyle style = new ChatStyle().setChatClickEvent(new ClickEvent(Action.RUN_COMMAND, "/tpr "+m.group(1)));
+			event.message.setChatStyle(style);
+		};
+    }
+}
