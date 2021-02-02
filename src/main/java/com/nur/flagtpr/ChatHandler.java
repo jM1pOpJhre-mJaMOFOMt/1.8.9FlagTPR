b4a2e9f3cf770171b70d8da2f9158d960488a1a7
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2021-02-01 23:37:12 -0800

    Fixed source indentation

--- src/main/java/com/nur/flagtpr/ChatHandler.java
+++ src/main/java/com/nur/flagtpr/ChatHandler.java
@@ -1,23 +1,22 @@
 package com.nur.flagtpr;
 
-
 import net.minecraft.event.ClickEvent;
 import net.minecraft.event.ClickEvent.Action;
 import net.minecraft.util.ChatStyle;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 import java.util.regex.*;
 
 public class ChatHandler {
-	@SubscribeEvent
-    public void onOtherChat(ClientChatReceivedEvent event)
-    {
-		String flagRegexp = "^\\[.+\\] (\\w+) VL \\d+";
-		Pattern flagPattern = Pattern.compile(flagRegexp);
-		Matcher m = flagPattern.matcher(event.message.getUnformattedText());
-		if(m.find()) {
-			ChatStyle style = new ChatStyle().setChatClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/tpr "+m.group(1)));
-			event.message.setChatStyle(style);
-		}
+    @SubscribeEvent
+    public void onOtherChat(ClientChatReceivedEvent event) {
+        String flagRegexp = "^\\[.+\\] (\\w+) VL \\d+";
+        Pattern flagPattern = Pattern.compile(flagRegexp);
+        Matcher m = flagPattern.matcher(event.message.getUnformattedText());
+        if (m.find()) {
+            ChatStyle style = new ChatStyle().setChatClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/tpr " + m.group(1)));
+            event.message.setChatStyle(style);
+        }
     }
 }
--- src/main/java/com/nur/flagtpr/Main.java
+++ src/main/java/com/nur/flagtpr/Main.java
@@ -6,8 +6,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
-public class Main
-{
+public class Main {
     public static final String MODID = "flagtpr";
     public static final String VERSION = "1.0";
     
