2d96fb085e34324a310c5b603bc5e5367289d49c
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2021-02-19 17:32:48 -0800

    Update formatting

--- src/main/java/com/nur/flagtpr/Main.java
+++ src/main/java/com/nur/flagtpr/Main.java
@@ -9,10 +9,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class Main {
     public static final String MODID = "flagtpr";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    	MinecraftForge.EVENT_BUS.register(new ChatHandler());
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ChatHandler());
     }
 }
